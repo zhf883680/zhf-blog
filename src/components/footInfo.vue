0bb4a96ca87046e3f38fee43777847a9a89d0de9
zhf883680 <609264462@qq.com>
2017-05-03 21:57:45 +0800

    add a little

--- src/components/footInfo.vue
+++ src/components/footInfo.vue
@@ -2,7 +2,7 @@
 	<div>
 		<el-row type="flex" justify="center">
 			<h5>
-			© 2017 - zhf 苏ICP备16065528号 联系方式:<a href="mailto:zhf883680@gmail.com">zhf883680@gmail.com</a>
+			© 2017 - zhf 苏ICP备16065528号 联系方式:<a href="mailto:zhf883680@gmail.com">zhf883680@gmail.com</a>  <a href="/login">后台登陆</a>
 			</h5>
 		</el-row>
 	</div>
