680c0a1e21dee3533041818bb472e8b358f88e56
zhf883680 <609264462@qq.com>
2017-05-04 13:20:46 +0800

    change css

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,67 +1,63 @@
 <template>
- <div>
- <el-row type="flex" justify="center">
-	<h2>{{item.Title}}</h2>
-	</el-row>
-	<el-row type="flex" justify="center">
-		  <p>{{changeTime(item.AddTime)}}</p>
-	</el-row>
-	<el-row type="flex" justify="start">
-		<el-col :offset="6" v-html="item.Content"> 
-		</el-col>
-	</el-row>
- </div>	
+    <div>
+        <el-row type="flex" justify="center">
+            <h2>{{item.Title}}</h2>
+        </el-row>
+        <el-row type="flex" justify="center">
+            <p>{{changeTime(item.AddTime)}}</p>
+        </el-row>
+        <el-row type="flex" justify="start">
+            <el-col :offset="6" v-html="item.Content">
+            </el-col>
+        </el-row>
+    </div>
 </template>
 <script>
 import axios from "axios"
-export default{
-	data(){
-		return{item:new Array};
-	},
-       mounted() {
-		axios.post("http://localhost:5707/Article/GetArticleInfo","id="+this.$route.params.id).then((data)=>{this.item = data.data})
-		},
-	methods:{
-		changeTime(date){
-		if (date == undefined) {
-            return "";
-        }
-        /*json格式时间转js时间格式*/
-        date = date.substr(1, date.length - 2);
-        let obj = eval('(' + "{Date: new " + date + "}" + ')');
-         date = obj["Date"];
-        if (date.getFullYear() < 1900) {
-            return "";
-        }
-        let datetime = date.getFullYear()
-                + "-"// "年"
-                + ((date.getMonth() + 1) >= 10 ? (date.getMonth() + 1) : "0"
-                        + (date.getMonth() + 1))
-                + "-"// "月"
-                + (date.getDate() < 10 ? "0" + date.getDate() : date
-                        .getDate())
-                + " "
-                + (date.getHours() < 10 ? "0" + date.getHours() : date
-                        .getHours())
-                + ":"
-                + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date
-                        .getMinutes())
-                + ":"
-                + (date.getSeconds() < 10 ? "0" + date.getSeconds() : date
+export default {
+    data() {
+            return {
+                item: new Array
+            };
+        },
+        mounted() {
+            this.getData();
+        },
+        methods: {
+            changeTime(date) {
+                if (date == undefined) {
+                    return "";
+                }
+                /*json格式时间转js时间格式*/
+                date = date.substr(1, date.length - 2);
+                let obj = eval('(' + "{Date: new " + date + "}" + ')');
+                date = obj["Date"];
+                if (date.getFullYear() < 1900) {
+                    return "";
+                }
+                let datetime = date.getFullYear() + "-" // "年"
+                    + ((date.getMonth() + 1) >= 10 ? (date.getMonth() + 1) : "0" + (date.getMonth() + 1)) + "-" // "月"
+                    + (date.getDate() < 10 ? "0" + date.getDate() : date
+                        .getDate()) + " " + (date.getHours() < 10 ? "0" + date.getHours() : date
+                        .getHours()) + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date
+                        .getMinutes()) + ":" + (date.getSeconds() < 10 ? "0" + date.getSeconds() : date
                         .getSeconds());
-        return datetime;
-		}
-	},
-	watch: {
-	  '$route' (to, from) {
-	  	//页面传递参数变化时,触发此方法
-	  	//获取文章内容,并且增加阅读次数
-		axios.post("/Article/GetArticleInfo","id="+this.$route.params.id).then((data)=>{this.item = data.data})
-	  }
-	}
+                return datetime;
+            },
+            getData(){
+                 axios.post("/Article/GetArticleInfo", "id=" + this.$route.params.id).then((data) => {
+                    this.item = data.data
+                })
+            }
+        },
+        watch: {
+            '$route' (to, from) {
+                //页面传递参数变化时,触发此方法
+                //获取文章内容,并且增加阅读次数
+                this.getData();
+            }
+        }
 }
 </script>
-
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
@@ -9,17 +8,16 @@ import router from './router'
 Vue.use(ElementUI);
 
 Vue.use(Lazyload, {
-        error: '../static/img/loading-2.gif',
-        loading: '../static/img/loading-2.gif',
-        try: 3 // default 1
-    })
-
+	error: '../static/img/loading-2.gif',
+	loading: '../static/img/loading-2.gif',
+	try: 3 // default 1
+})
 Vue.config.productionTip = false
-
-
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+	el: '#app',
+	router,
+	template: '<App/>',
+	components: {
+		App
+	}
 })
